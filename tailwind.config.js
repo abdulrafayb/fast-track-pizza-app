dd282ce37c3218ffa069918e4257bfb98894f7e0
abdulrafayb <abdulrafay200066@gmail.com>
2025-07-06 21:40:54 +0500

    done with styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      // mono: "Roboto Mono, monospace",
+      sans: "Roboto Mono, monospace",
+    },
     extend: {},
   },
   plugins: [],
 };
+
+/* if we want to use the above font everywhere and not at specified places like giving class name of font-mono we just put it in sans to override the sans font that tailwind automatically injects into our HTML */
